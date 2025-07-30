175aedb7cf5b7ab503f738efdcdff5d243d860de
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-07-30 13:47:44 +0300

    Added 10.3

---
+++ rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import Constants from "expo-constants";
+import { StyleSheet, View } from "react-native";
+import RepositoriyList from "./RepositoryList";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoriyList></RepositoriyList>
+    </View>
+  );
+};
+
+export default Main;

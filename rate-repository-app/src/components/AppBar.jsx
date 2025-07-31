e1fb9a41105494addebebadcfd610b3ce46f795e
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-07-31 08:58:24 +0300

    Sign in page added

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -2,6 +2,7 @@ import { View, StyleSheet, Pressable } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,12 +15,32 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   console.log("BVDFG");
   return (
-    <View style={[styles.container, theme.backgrounds.bar]}>
-      <Pressable style={theme.backgrounds.bars}>
-        <Text style={{ color: "white", fontWeight: "bold", padding: 8 }}>
-          Repositories
-        </Text>
-      </Pressable>
+    <View
+      style={{
+        ...styles.container,
+        ...theme.backgrounds.bar,
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <View style={[(display = "flex"), (flexDirection = "column")]}>
+        <Pressable style={theme.backgrounds.bar}>
+          <Link to="/">
+            <Text style={{ color: "white", fontWeight: "bold", padding: 8 }}>
+              Repositories
+            </Text>
+          </Link>
+        </Pressable>
+      </View>
+      <View style={[(display = "flex"), (flexDirection = "column")]}>
+        <Pressable style={theme.backgrounds.bar}>
+          <Link to="/signin">
+            <Text style={{ color: "white", fontWeight: "bold", padding: 8 }}>
+              Sign in
+            </Text>
+          </Link>
+        </Pressable>
+      </View>
     </View>
   );
 };

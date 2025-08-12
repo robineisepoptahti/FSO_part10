f5b29bfea8dc1ce1b9eb4b79565d495b0327cb2f
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-08-12 15:00:07 +0300

    10.11 added

---
+++ rate-repository-app/src/graphql/queries.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          fullName
+          language
+          reviewCount
+          forksCount
+          description
+          stargazersCount
+          ownerAvatarUrl
+        }
+      }
+    }
+  }
+`;
---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const { data, loading, error } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+    // Other options
+  });
+  console.log("useQuery called:", { data, loading, error });
+  if (error) {
+    console.error("GraphQL Error:", error);
+  }
+
+  return {
+    repositories: data?.repositories,
+    loading,
+    error,
+  };
+};
+
+export default useRepositories;

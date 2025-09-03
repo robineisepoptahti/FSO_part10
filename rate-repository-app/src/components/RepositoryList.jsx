ff1c3d8f9a562ac89da9d762a251ee81c240ad18
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-03 11:28:21 +0300

    Added 10.23

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -2,6 +2,8 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
+import { useState, useEffect } from "react";
+import { Picker } from "@react-native-picker/picker";
 
 const styles = StyleSheet.create({
   separator: {
@@ -12,7 +14,46 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+const SortingMenu = ({ selectedSortingMethod, setSelectedSortingMethod }) => {
+  return (
+    <View style={{ backgroundColor: "grey" }}>
+      <Picker
+        selectedValue={selectedSortingMethod}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedSortingMethod(itemValue)
+        }
+      >
+        <Picker.Item
+          label="Select an item..."
+          value="select"
+          enabled={false}
+          color={"gray"}
+        />
+        <Picker.Item
+          label="Latest repositories"
+          value="CREATED_AT"
+          color="black"
+        />
+        <Picker.Item
+          label="Highest rated repositories"
+          value="RATING_AVERAGE_ASC"
+          color="black"
+        />
+        <Picker.Item
+          label="Lowest rated repositories"
+          value="RATING_AVERAGE_DESC"
+          color="black"
+        />
+      </Picker>
+    </View>
+  );
+};
+
+export const RepositoryListContainer = ({
+  repositories,
+  selectedSortingMethod,
+  setSelectedSortingMethod,
+}) => {
   const navigate = useNavigate();
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
@@ -26,14 +67,32 @@ export const RepositoryListContainer = ({ repositories }) => {
           <RepositoryItem item={item} />
         </Pressable>
       )}
+      ListHeaderComponent={() => (
+        <SortingMenu
+          selectedSortingMethod={selectedSortingMethod}
+          setSelectedSortingMethod={setSelectedSortingMethod}
+        ></SortingMenu>
+      )}
     />
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [selectedSortingMethod, setSelectedSortingMethod] =
+    useState("CREATED_AT");
+  const orderDirection =
+    selectedSortingMethod === "RATING_AVERAGE_ASC" ? "DESC" : "ASC";
+  const orderBy =
+    selectedSortingMethod === "CREATED_AT" ? "CREATED_AT" : "RATING_AVERAGE";
+  const { repositories } = useRepositories(orderBy, orderDirection);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      selectedSortingMethod={selectedSortingMethod}
+      setSelectedSortingMethod={setSelectedSortingMethod}
+    />
+  );
 };
 
 export default RepositoryList;

d84b0bf61bb23a7b75b6984223496a072698a458
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-09-08 12:42:27 +0300

    10.24, search added

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -2,8 +2,9 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Picker } from "@react-native-picker/picker";
+import { Searchbar } from "react-native-paper";
 
 const styles = StyleSheet.create({
   separator: {
@@ -14,9 +15,20 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const SortingMenu = ({ selectedSortingMethod, setSelectedSortingMethod }) => {
+const SortingMenu = ({
+  selectedSortingMethod,
+  setSelectedSortingMethod,
+  searchKeyword,
+  setSearchKeyword,
+}) => {
   return (
     <View style={{ backgroundColor: "grey" }}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={setSearchKeyword}
+        value={searchKeyword}
+        style={{ margin: 15 }}
+      />
       <Picker
         selectedValue={selectedSortingMethod}
         onValueChange={(itemValue, itemIndex) =>
@@ -49,48 +61,62 @@ const SortingMenu = ({ selectedSortingMethod, setSelectedSortingMethod }) => {
   );
 };
 
-export const RepositoryListContainer = ({
-  repositories,
-  selectedSortingMethod,
-  setSelectedSortingMethod,
-}) => {
-  const navigate = useNavigate();
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-        <Pressable onPress={() => navigate(`/repository/${item.id}`)}>
-          <RepositoryItem item={item} />
-        </Pressable>
-      )}
-      ListHeaderComponent={() => (
-        <SortingMenu
-          selectedSortingMethod={selectedSortingMethod}
-          setSelectedSortingMethod={setSelectedSortingMethod}
-        ></SortingMenu>
-      )}
-    />
-  );
-};
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    const props = this.props;
+    return (
+      <SortingMenu
+        selectedSortingMethod={props.selectedSortingMethod}
+        setSelectedSortingMethod={props.setSelectedSortingMethod}
+        searchKeyword={props.searchKeyword}
+        setSearchKeyword={props.setSearchKeyword}
+      ></SortingMenu>
+    );
+  };
+  render() {
+    const repositoryNodes = this.props.repositories
+      ? this.props.repositories.edges.map((edge) => edge.node)
+      : [];
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => this.props.navigate(`/repository/${item.id}`)}
+          >
+            <RepositoryItem item={item} />
+          </Pressable>
+        )}
+        ListHeaderComponent={this.renderHeader}
+      />
+    );
+  }
+}
 
 const RepositoryList = () => {
+  const navigate = useNavigate();
   const [selectedSortingMethod, setSelectedSortingMethod] =
     useState("CREATED_AT");
+  const [searchKeyword, setSearchKeyword] = useState("");
   const orderDirection =
     selectedSortingMethod === "RATING_AVERAGE_ASC" ? "DESC" : "ASC";
   const orderBy =
     selectedSortingMethod === "CREATED_AT" ? "CREATED_AT" : "RATING_AVERAGE";
-  const { repositories } = useRepositories(orderBy, orderDirection);
+  const { repositories } = useRepositories(
+    orderBy,
+    orderDirection,
+    searchKeyword
+  );
 
   return (
     <RepositoryListContainer
       repositories={repositories}
       selectedSortingMethod={selectedSortingMethod}
       setSelectedSortingMethod={setSelectedSortingMethod}
+      searchKeyword={searchKeyword}
+      setSearchKeyword={setSearchKeyword}
+      navigate={navigate}
     />
   );
 };
